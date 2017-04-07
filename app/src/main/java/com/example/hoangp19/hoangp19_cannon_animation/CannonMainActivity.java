796ae789b2b64734fc0c3c70e172b430993926ee
hoangpaul123 <hoangp19@up.edu>
2017-04-06 19:53:21 -0700

    got app to appear on app

--- app/src/main/java/com/example/hoangp19/hoangp19_cannon_animation/CannonMainActivity.java
+++ app/src/main/java/com/example/hoangp19/hoangp19_cannon_animation/CannonMainActivity.java
@@ -4,21 +4,10 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.widget.LinearLayout;
-
-/**
- * CannonMainActivity
- *
- * This is the activity for the cannon animation. It creates a AnimationCanvas
- * containing a particular Animator object
- *
- * @author Andrew Nuxoll
- * @version September 2012
- *
- */
 public class CannonMainActivity extends Activity {
 
     /**
-     * creates an AnimationCanvas containing a TestAnimator.
+     * creates an AnimationCanvas containing an Animator.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,13 +15,17 @@ public class CannonMainActivity extends Activity {
         setContentView(R.layout.activity_cannon_main);
 
         // Create an animation canvas and place it in the main layout
-        Animator testAnim = new TestAnimator();
-        AnimationCanvas myCanvas = new AnimationCanvas(this, testAnim);
+        Animator animator = new DrawCannon(this);
+        AnimationCanvas myCanvas = new AnimationCanvas(this, animator);
         LinearLayout mainLayout = (LinearLayout) this
                 .findViewById(R.id.topLevelLayout);
         mainLayout.addView(myCanvas);
 
     }
+
+    /**
+     * This is the default behavior (empty menu)
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.cannon_main, menu);
